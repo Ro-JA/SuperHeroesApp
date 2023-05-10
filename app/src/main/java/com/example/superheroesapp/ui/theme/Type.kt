9d302080e0e0ace037c4080fb0dfe362b043f77b
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-05-10 22:52:37 +0900

    add res theme

--- app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
@@ -1,36 +1,43 @@
 package com.example.superheroesapp.ui.theme
 
 import androidx.compose.material3.Typography
+import com.example.superheroesapp.R
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.superheroesapp.R
 
-val CabinBold = FontFamily(
-    Font(R.font.cabin_bold)
-)
 
-val CabinRegular = FontFamily(
-    Font(R.font.cabin_regular)
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular, FontWeight.Normal),
+    Font(R.font.cabin_bold, FontWeight.Bold)
 )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
 
-    titleLarge = TextStyle(
-        fontFamily = CabinBold,
+    bodyLarge = TextStyle(
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = CabinRegular,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     )
 )
