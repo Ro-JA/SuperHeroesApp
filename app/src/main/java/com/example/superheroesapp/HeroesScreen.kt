f2f79253f2d427bf3589771bb19cc6174c9f7c6e
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-05-13 11:48:09 +0900

    add lazyColum

--- app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
@@ -1,21 +1,22 @@
 package com.example.superheroesapp
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,6 +28,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroesapp.model.Hero
+import com.example.superheroesapp.model.HeroesRepository
 import com.example.superheroesapp.ui.theme.SuperheroesTheme
 
 @Composable
@@ -56,18 +58,35 @@ fun HeroItem(
                 )
             }
             Spacer(modifier = Modifier.width(16.dp))
-            Box(modifier = Modifier
-                .size(72.dp)
-                .clip(RoundedCornerShape(8.dp))) {
+            Box(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
                 Image(
                     painter = painterResource(id = hero.imageRes),
                     contentDescription = null,
-                alignment = Alignment.TopCenter,
-                contentScale = ContentScale.FillWidth )
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
             }
         }
 
     }
+}
+
+@Composable
+fun HeroesList(
+    heroes: List<Hero>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn {
+        itemsIndexed(heroes) { index, hero ->
+            HeroItem(hero = hero,
+            modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp))
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
@@ -81,6 +100,18 @@ fun HeroItemPreview() {
     SuperheroesTheme() {
         HeroItem(hero = hero)
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroesList() {
+    SuperheroesTheme() {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            HeroesList(heroes = HeroesRepository.heroes)
+        }
+        
     }
+}
+    
 
 
