4a9c79a6535d4fbd2b8f53b6f8eb67853886c135
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-05-13 11:27:53 +0900

    work item

--- app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
@@ -12,12 +12,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,8 +56,14 @@ fun HeroItem(
                 )
             }
             Spacer(modifier = Modifier.width(16.dp))
-            Box(modifier = modifier) {
-                Image(painter = painterResource(id = hero.imageRes), contentDescription = null)
+            Box(modifier = Modifier
+                .size(72.dp)
+                .clip(RoundedCornerShape(8.dp))) {
+                Image(
+                    painter = painterResource(id = hero.imageRes),
+                    contentDescription = null,
+                alignment = Alignment.TopCenter,
+                contentScale = ContentScale.FillWidth )
             }
         }
 
