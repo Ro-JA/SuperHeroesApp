023d62ece16c0768696d3b6f2e60a16bf0bbd86e
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-05-13 11:12:23 +0900

    add modifier, not work

--- app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
@@ -1,38 +1,76 @@
 package com.example.superheroesapp
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.superheroesapp.model.Hero
+import com.example.superheroesapp.ui.theme.SuperheroesTheme
 
 @Composable
 fun HeroItem(
     hero: Hero,
     modifier: Modifier = Modifier
 ) {
-Card(modifier = modifier) {
-    Row() {
-        HeroInformation(nameHero = hero.nameRes, descriptionHero = hero.descriptionRes)
+    Card(
+        // подъем карты над основыным слоем
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(id = hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall
+                )
+                Text(
+                    text = stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box(modifier = modifier) {
+                Image(painter = painterResource(id = hero.imageRes), contentDescription = null)
+            }
+        }
+
     }
-}
 }
 
+@Preview(showBackground = true)
 @Composable
-fun HeroInformation(
-    @StringRes nameHero: Int,
-    @StringRes descriptionHero: Int,
-    modifier: Modifier = Modifier
-) {
-    Column(modifier = modifier) {
-        Text(text = stringResource(id = nameHero),
-        style = MaterialTheme.typography.displayLarge)
-        Text(text = stringResource(id = descriptionHero),
-        style = MaterialTheme.typography.displayMedium)
+fun HeroItemPreview() {
+    val hero = Hero(
+        R.string.hero1,
+        R.string.description1,
+        R.drawable.android_superhero1
+    )
+    SuperheroesTheme() {
+        HeroItem(hero = hero)
     }
-}
+    }
+
+
